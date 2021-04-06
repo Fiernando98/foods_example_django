eb6cb8833f26475e5cb5652db84450bb1dd365ea
Fernando <fiernando_lerma@hotmail.com>
2021-04-06 16:56:21 -0600

    valio queso mi esfuerzo

--- food_api/food_app/views.py
+++ food_api/food_app/views.py
@@ -7,60 +7,8 @@ from rest_framework.exceptions import NotFound
 from rest_framework import status, viewsets
 
 
-class FoodViewSet(viewsets.ViewSet):
+class FoodViewSet(viewsets.ModelViewSet):
     model = Food
     serializer_class = FoodSerializer
+    queryset = model.objects.all()
 
-    def list(self, request):
-        object_class = self.model.objects.all()
-        query_name = self.request.GET.get('name')
-        if query_name is not None:
-            object_class = object_class.filter(name__contains=query_name)
-        serializer = self.serializer_class(object_class, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def retrieve(self, request, pk):
-        try:
-            object_class = self.model.objects.get(pk=pk)
-        except ObjectDoesNotExist:
-            raise NotFound(detail=('That Food does not exist.'))
-        serializer = self.serializer_class(object_class)
-        return Response(serializer.data)
-
-    def update(self, request, *args, **kwargs):
-        try:
-            instance = self.model.objects.get(pk=kwargs.get('pk'))
-        except self.model.DoesNotExist:
-            raise NotFound(detail=('That Food does not exist.'))
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=ValueError):
-            serializer.update(instance=instance, validated_data=request.data)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
-
-    def partial_update(self, request, *args, **kwargs):
-        try:
-            instance = self.model.objects.get(pk=kwargs.get('pk'))
-        except self.model.DoesNotExist:
-            raise NotFound(detail=('That Food does not exist.'))
-        serializer = self.serializer_class(instance, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=ValueError):
-            serializer.update(instance=instance, validated_data=request.data)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk):
-        try:
-            object_class = self.model.objects.get(pk=pk)
-        except ObjectDoesNotExist:
-            raise NotFound(detail=('That Food does not exist.'))
-        object_class.delete()
-        return Response({
-            'detail': 'That Food is already deleted'
-        }, status=status.HTTP_200_OK)
